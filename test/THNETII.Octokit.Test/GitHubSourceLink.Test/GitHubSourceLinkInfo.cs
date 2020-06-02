41f024ff4c1e285c4f7d352f5e748e5859e61a9c
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-06-02 06:05:18 +0200

    Add unit test for getting repository contents from GitHub using source link information

---
+++ test/THNETII.Octokit.Test/GitHubSourceLink.Test/GitHubSourceLinkInfo.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+using Octokit.Test;
+
+namespace Octokit.GitHubSourceLink.Test
+{
+    public static class GitHubSourceLinkInfo
+    {
+        internal static string FilePath { get; } =
+            typeof(GitHubSourceLinkInfo).FullName + ".json";
+
+        public static string? GetGitShaFromAssemblyVersion()
+        {
+            string? sha = null;
+            if (typeof(GitHubSourceLinkInfo).Assembly
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion is string version)
+            {
+                int idx = version.IndexOf('+', StringComparison.Ordinal);
+                if (idx >= 0)
+                    sha = version.Substring(idx + 1);
+            }
+            return sha;
+        }
+
+        public static IConfigurationBuilder AddGitHubSourceLinkInfo(
+            this IConfigurationBuilder config)
+        {
+            const string baseKey = nameof(GitHubSourceLink);
+            return config
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    [ConfigurationPath.Combine(baseKey, nameof(GitHubSourceLinkOptions.Owner))] = "thnetii",
+                    [ConfigurationPath.Combine(baseKey, nameof(GitHubSourceLinkOptions.Repository))] = "octokit.net-extensions",
+                    [ConfigurationPath.Combine(baseKey, nameof(GitHubSourceLinkOptions.Reference))] = GetGitShaFromAssemblyVersion(),
+                })
+                .AddJsonFile(EmbeddedFiles.Provider, FilePath,
+                    optional: true, reloadOnChange: false);
+        }
+
+        public static IServiceCollection AddGitHubSourceLinkOptions(
+            this IServiceCollection services)
+        {
+            services.AddOptions<GitHubSourceLinkOptions>()
+                .Configure<IConfiguration>((options, config) =>
+                    config.Bind(nameof(GitHubSourceLink), options)
+                    )
+                .Validate(options => options.Validate())
+                ;
+
+            return services;
+        }
+                
+    }
+
+    public class GitHubSourceLinkOptions
+    {
+        public string Owner { get; set; }
+        public string Repository { get; set; }
+        public string Reference { get; set; }
+
+        [SuppressMessage("Usage", "CA2208: Instantiate argument exceptions correctly", Justification = nameof(Options))]
+        public bool Validate()
+        {
+            if (Owner is null)
+                throw new ArgumentNullException(nameof(Owner));
+            if (Repository is null)
+                throw new ArgumentNullException(nameof(Repository));
+
+            return true;
+        }
+    }
+}
---
+++ test/THNETII.Octokit.Test/GitHubSourceLink.Test/GitHubSourceLinkTest.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+using Octokit.Authentication.Test;
+using Octokit.Test;
+
+using Xunit;
+
+namespace Octokit.GitHubSourceLink.Test
+{
+    public static class GitHubSourceLinkTest
+    {
+        [Fact]
+        public static void GetsRepositoryContents()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<IConfiguration>(new ConfigurationBuilder()
+                .AddAuthSecrets()
+                .AddGitHubSourceLinkInfo()
+                .Build());
+            services.AddOctokitCredentials();
+            services.AddGitHubSourceLinkOptions();
+
+            using var provider = services.BuildServiceProvider();
+
+            var client = new GitHubClient(
+                AssemblyProductHeaderValue.Instance,
+                provider.GetRequiredService<ICredentialStore>());
+
+            var sourceLink = provider.GetRequiredService<IOptions<GitHubSourceLinkOptions>>()
+                .Value;
+
+            Task<IReadOnlyList<RepositoryContent>> contentTask =
+                sourceLink.Reference is null
+                ? client.Repository.Content.GetAllContents(
+                    sourceLink.Owner,
+                    sourceLink.Repository)
+                : client.Repository.Content.GetAllContentsByRef(
+                    sourceLink.Owner,
+                    sourceLink.Repository,
+                    sourceLink.Reference);
+            var contents = contentTask.ConfigureAwait(false)
+                .GetAwaiter().GetResult();
+
+            Assert.NotEmpty(contents);
+        }
+    }
+}
